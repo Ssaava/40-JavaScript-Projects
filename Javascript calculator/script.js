32d88e298bbc942216727499e8c5a0fe1a807251
RAHMAH <nanyongarahmah891@gmail.com>
2023-05-03 17:12:40 +0300

    added a few functionalities to Calculator

--- Javascript calculator/script.js
+++ Javascript calculator/script.js
@@ -1,10 +1,26 @@
 const answerSection = document.getElementById("answer");
-let numbers = document.querySelectorAll(".btn");
-function getButton() {
-  console.log(numbers);
-  numbers.forEach((event) => {
-    if (event.innerHTML == "7") {
-      answerSection.value = event.innerHTML;
+let Buttons = document.querySelector(".buttons");
+var operators = /\W/g;
+// function to capture user input
+var numbers = "";
+
+Buttons.addEventListener("click", (e) => {
+  let number = e.target.textContent;
+  var lastCharactor = numbers.charAt(numbers.length - 1);
+  if (lastCharactor.match(operators)) {
+    console.log("Character matches");
+  }
+  if (number >= 0 || number === "." || number.match(operators)) {
+    if (number === "0" && answerSection.value === "0") {
+      answerSection.value = "0";
+      return;
     }
-  });
-}
+
+    numbers += e.target.textContent;
+    answerSection.value = numbers;
+  }
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  answerSection.value = "";
+});
