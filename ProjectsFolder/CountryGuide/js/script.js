e88658a1c566e97e6157566ea9d79836a9d6bbab
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2023-07-08 10:53:42 +0300

    Done with the cards section design

--- ProjectsFolder/CountryGuide/js/script.js
+++ ProjectsFolder/CountryGuide/js/script.js
@@ -24,3 +24,21 @@ Your users should be able to:
 - Click through to the border countries on the detail page
 - Toggle the color scheme between light and dark mode *(optional)*
  */
+
+const container = document.querySelector(".container-fluid");
+const our_html =  "<div class='card'>
+<img
+  src='https://flagcdn.com/ax.svg'
+  class='card-img-top'
+  alt='...'
+/>
+<div class='card-body'>
+  <h5 class='card-title fw-bold w-100'>Uganda</h5>
+  <ul class='card-text fw-light'>
+    <li>Population: <span>420000</span></li>
+    <li>Region: <span>Uganda</span></li>
+    <li>Capital: <span>Kampala</span></li>
+  </ul>
+</div>
+</div>
+"
